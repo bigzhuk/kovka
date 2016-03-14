60907e4f4c31bb67fda5e8c9b4119d1d58fa985e
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2016-03-14 19:19:22 +0300

    Вывод рамндомного текста рекламного блока из числа доступных.

--- classes/News/Decorator/BlockKovkaFront.php
+++ classes/News/Decorator/BlockKovkaFront.php
@@ -3,6 +3,10 @@ namespace News\Decorator;
 
 class BlockKovkaFront extends News {
    public function renderReklamniyBlock(array $new_rows) {
-        var_dump($new_rows);
+       if(empty($new_rows)) {
+           return '';
+       }
+       $show_item_number = rand(0, count($new_rows)-1);
+       var_dump($new_rows[$show_item_number]);
     }
 }
