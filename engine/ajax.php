7a9bdebcc573209b6bdb086630ed7532cc9fb7e9
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2016-07-05 14:32:10 +0300

    Поправил конфиг бд

--- engine/ajax.php
+++ engine/ajax.php
@@ -3,16 +3,16 @@
 	include('config.php');
 
 	# Перенаправление на пользовательскую функцию
-	if (isset($_POST['action']) && $_POST['action']!==''){
+	if (isset($_POST['action']) && $_POST['action']!=='') {
 		$action = $_POST['action'];
 		$result = $action();
 		echo(json_encode($result));
 	}
 
-	function recall(){
+	function recall() {
 		$phone = $_POST['phone'];
 		$name = $_POST['name'];
-		$mailto = 'design@spartak.ru';
+		$mailto = 'sus-stroy@mail.ru';
 		$subject = 'Обратный звонок';
 		$message = 'Обратный звонок '.$phone;
 		if (sendMail($mailto, $subject, $message)){
