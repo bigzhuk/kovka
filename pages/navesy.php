a637209dae616a25aca0fea51e8c28e2bbe1924c
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2016-07-26 22:43:28 +0300

    добавил фоток на страницу навесы

--- pages/navesy.php
+++ pages/navesy.php
@@ -2,6 +2,7 @@
 <h1>Навесы из поликарбоната</h1>
 
 <div class="container">
+	<img src="images/navesy_big/navesi_iz_polikorbonata.jpg">
 	<p>Поликарбонат – современный материал, вобравший в себя преимущества стекла
 		(прозрачность, визуальная легкость, высокая светопроницаемость), но не обладающий его недостатками,
 		главные из которых - хрупкость и большой вес.  Изготовление навесов из этого материала позволило получить практичную
@@ -16,25 +17,27 @@
 	<p>Исходя из конструкционных особенностей выделяют следующие <strong>категории навесов</strong>:</p>
 	<div>
 		<div style="float: left;">
-			<img src="images/navesy_small/naves_odnoskatniy.jpg" style="width: 250px; heifht: 125;  margin-left: 15px; border-radius: 3px;">
+			<img src="images/navesy_small/naves_odnoskatniy.jpg" style="width: 250px; height: 125px;  margin-left: 15px; border-radius: 3px;">
 			<div align="center">Односкатные. <span style="color: #C42034">От 2400 за м²</span></div>
 		</div>
 		<div style="float: left;">
-			<img src="images/navesy_small/naves_dvuskatniy.jpg" style="width: 250px; heifht: 125;  margin-left: 15px; border-radius: 3px;">
+			<img src="images/navesy_small/naves_dvuskatniy.jpg" style="width: 250px; height: 125px;  margin-left: 15px; border-radius: 3px;">
 			<div align="center">Двускатные. <span style="color: #C42034">От 2500 за м²</span></div>
 		</div>
 		<div style="float: left;">
-			<img src="images/navesy_small/naves_arochniy.jpg" style="width: 250px; heifht: 125;  margin-left: 15px; border-radius: 3px;">
+			<img src="images/navesy_small/naves_arochniy.jpg" style="width: 250px; height: 125px;  margin-left: 15px; border-radius: 3px;">
 			<div align="center">Арочные. <span style="color: #C42034">От 2500 за м²</span></div>
 		</div>
 		<div style="float: left;">
-			<img src="images/navesy_small/naves_kovanniy.jpg" style="width: 250px; heifht: 125;  margin-left: 15px; border-radius: 3px;">
+			<img src="images/navesy_small/naves_kovanniy.jpg" style="width: 250px; height: 125px;  margin-left: 15px; border-radius: 3px;">
 			<div align="center">Кованные. <span style="color: #C42034">От 4500 за м²</span></div>
 		</div>
 	</div>
 	<div style="clear: both"></div>
 	<h2>Навесы для автомобиля</h2>
-	<p>Защитные сооружения из металлического каркаса, покрытого сотовым поликарбонатом – практичное и выгодное решение
+	<p>
+		<img src="images/navesy_big/naves_iz_polikarbonata_dlya_avto.jpg" class="left" style="width: 200px; height: 150px; margin-bottom: 5px;">
+		Защитные сооружения из металлического каркаса, покрытого сотовым поликарбонатом – практичное и выгодное решение
 		для владельцев машин и автостоянок. Такая конструкция отличается долговечностью и хорошим внешним
 		видом, не требует серьезных вложений.
 		Прочные <strong>автомобильные навесы</strong> из поликарбоната удачно вписываются в современный ландшафтный дизайн
@@ -45,15 +48,18 @@
 
 	<h2>Навесы для бассейнов</h2>
 	<p>
+		<img src="images/navesy_big/naves_iz_polikarbonata_dlya_bassejna.jpg" class="left" style="width: 200px; height: 150px; margin-bottom: 5px;">
 		<strong>Павильон для бассейнов</strong> является достаточно комфортабельным инженерным сооружением, которое помимо своей функциональности
 		дополнительно служит в качестве декорационного дополнения к жилой территории.
 		Не смотря на то, что технология изготовления навесов для бассейнов из поликарбоната достаточно нова,
 		она  быстро получила распростарнение. Наша компания возводит <strong>навесы для бассейна</strong>
 		любой сложности по доступным ценам.
+		<br/><br/><br/>
 	</p>
 
 	<h2>Навесы над крыльцом</h2>
 	<p>
+		<img src="images/navesy_big/naves_iz_polikarbonata_nad_kriltsom.jpg" class="left" style="width: 200px; height: 150px; margin-bottom: 5px;">
 		Театр начинается с вешалки, квартира с прихожей, а первое впечатление о доме создает козырек над крыльцом.
 		Классический элемент дизайна не только защищает вход в жилище. Он помогает создать его оригинальное оформление.
 		Мы создаем изысканные <strong>навесы над крыльцом</strong>, которые выгодно выделят ваш дом, придав ему уюта и шарма.
@@ -62,8 +68,9 @@
 		и кошелек.
 	</p>
 
-	<h2>Навесы для балконов</h2>
+	<h2>Навесы над балконом</h2>
 	<p>
+		<img src="images/navesy_big/naves_iz_polikarbonata_nad_balkonom.jpeg" class="left" style="width: 200px; height: 150px; margin-bottom: 5px;">
 		Отдых на балконе является распространенным способом провести свободное время во многих странах,
 		особенно, если с балкона открывается отличный вид. Красиво оформленный балкон, на котором стоит легкий
 		стеклянный стол и плетеное кресло станет вашим любимым местом, где вы сможете часами наслаждаться музыкой,
