074d438053c53851004dc10fee56b574d6fbd95d
g.grigorevsky <g.grigorevsky@b2b-center.ru>
2017-08-30 11:59:07 +0300

    Подключаем живосайт

--- index.php
+++ index.php
@@ -45,10 +45,14 @@ require_once('classes/autoload.php');
 		<div id="wrapper_push"></div>
 	</div>
 
-	<?php include('static/footer.php'); ?>	
+	<?php include('static/footer.php'); ?>
 
-	
 
+    <!-- BEGIN JIVOSITE CODE {literal} -->
+    <script type='text/javascript'>
+        (function(){ var widget_id = 'KPsBCKgV5U';var d=document;var w=window;function l(){
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
+    <!-- {/literal} END JIVOSITE CODE -->
 </body>
 </html>
 
